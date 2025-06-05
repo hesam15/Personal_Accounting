af83d234c008d63ad1b193f549a5bee550012859
Hesam <hesam.15ztr@gmail.com>
2025-06-05 23:06:52 +0330

    complete BudgetController logic

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Budget;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +14,13 @@ class BudgetController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+
+        $budgets = $user->budgets;
+
+        return response()->json([
+            'budgets' => $budgets
+        ]);
     }
 
     /**
@@ -21,30 +28,83 @@ class BudgetController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string|min:1|max:50|unique:budgets',
+                'amount' => 'required|integer|min:1000'
+            ]);
+
+            $budget = Budget::create([
+                'name' => $validated['name'],
+                'amount' => $validated['amount'],
+                'user_id' => $user->id
+            ]);
+
+            return response()->json([
+                'message' => "بودجه $budget->name با موفقیت ذخیره سازی شد",
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => 'ایجاد بودجه با ارور مواجه شد',
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Budget $budget)
     {
-        //
+        return response()->json([
+            'budget' => $budget
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Budget $budget)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string|min:1|max:50|unique:budgets',
+                'amount' => 'required|integer|min:1000'
+            ]);
+
+            $budget->update([
+                'name' => $validated['name'],
+                'amount' => $validated['amount']
+            ]);
+
+            return response()->json([
+                'message' => "آپدیت بودجه $budget->name با موفقیت انجام شد"
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => 'آپدیت بودجه با ارور مواجه شد',
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Budget $budget)
     {
-        //
+        try {
+            $budget->delete();
+
+            return response()->json([
+                'message' => "بودجه $budget->name با موفقیت حذف شد"
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => 'حذف بودجه با ارور مواجه شد',
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
