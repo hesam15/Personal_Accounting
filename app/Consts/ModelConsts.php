72ad8640d498e386d15e272a69ea9526dbdac3d8
Hesam <hesam.15ztr@gmail.com>
2025-06-14 17:29:52 +0330

    complete SaveBoxController logic

---
+++ app/Consts/ModelConsts.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Consts;
+
+class ModelConsts {
+    const BUDGET = 'App\Models\Budget';
+    const DAILYEXPENSES = 'App\Models\DailyExpenses';
+}
--- app/Http/Controllers/SaveBoxController.php
+++ app/Http/Controllers/SaveBoxController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SaveBox;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class SaveBoxController extends Controller
 {
@@ -11,7 +15,13 @@ class SaveBoxController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+
+        $saveBoxes = $user->saveBoxes;
+
+        return response()->json([
+            $saveBoxes
+        ]);
     }
 
     /**
@@ -19,30 +29,94 @@ class SaveBoxController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $user = Auth::user();
+
+            $validated = $request->validate([
+                'name' => ['required', 'string', 'max:50', Rule::unique('save_boxs')->where('user_id', $user->id)],
+                'amount' => 'required|integer|min:1000'
+            ]);
+
+            $saveBox = DB::transaction(function() use ($validated, $user){
+                $saveBox = SaveBox::create([
+                    'name' => $validated['name'],
+                    'amount' => $validated['amount'],
+                    'user_id' => $user->id
+                ]);
+                
+                return $saveBox;
+            });
+
+            return response()->json([
+                'message' => "باکس ذخیره $saveBox->name با موفقیت ثبت شد"
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => 'ذخیره باکس ذخیره با ارور مواجه شد، مجددا تلاش کنید',
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(SaveBox $saveBox)
     {
-        //
+        return response()->json([
+            $saveBox
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, SaveBox $saveBox)
     {
-        //
+        try {
+            $user = Auth::user();
+
+            $validated = $request->validate([
+                'name' => ['required', 'string', 'max:50', Rule::unique('save_boxs')->where('user_id', $user->id)->ignore($saveBox->id)],
+                'amount' => 'required|integer|min:1000'
+            ]);
+
+            DB::transaction(function() use ($validated, $saveBox){
+                $saveBox->update([
+                    'name' => $validated['name'],
+                    'amount' => $validated['amount']
+                ]);
+            });
+
+            return response()->json([
+                'message' => "باکس ذخیره '$saveBox->name' با موفقیت آپدیت شد"
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => "حذف $saveBox->name با ارور مواجه شد، مجددا تلاش کنید",
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(SaveBox $saveBox)
     {
-        //
+        $saveBoxName = $saveBox->name;
+
+        try {
+            $saveBox->delete();
+
+            return response()->json([
+                'message' => "باکس ذخیره '$saveBoxName' با موفقیت حذف شد"
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => "حذف باکس ذخیره '$saveBoxName' با ارور مواجه شد، مجددا تلاش کنید",
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
