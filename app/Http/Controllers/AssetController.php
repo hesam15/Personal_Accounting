ad8bc492cc204aeee5b9139a2e93826ff1b010e8
Hesam <hesam.15ztr@gmail.com>
2025-06-20 13:29:38 +0330

    add AssetResource and complete show and update logics in AssetController

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Asset;
 use Illuminate\Http\Request;
 
 class AssetController extends Controller
 {
-    //
+    public function show(Asset $asset) {
+        return $asset->toResource();
+    }
+
+    public function update(Asset $asset, Request $request) {
+        try {
+            $validated = $request->validate([
+                'amount' => 'required|integer'
+            ]);
+
+            $asset->update([
+                'amount' => $validated['amount']
+            ]);
+
+            return response()->json([
+                'message' => 'آپدیت موجودی با موفقیت انجام شد'
+            ]);
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => 'آپدیت موجودی با ارور مواجه شد، مجددا تلاش کنید',
+                'error' => $e->getMessage()
+            ], 422);
+        }
+    }
 }
